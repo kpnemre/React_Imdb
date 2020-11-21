bd5e9864fb0f74fcf8f02d9a2cc01f655876f881
sahinturk <kopuskopecik@gmail.com>
2020-11-21 07:11:26 +0200

    searching when button click is added

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,28 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Card } from "../Card";
 import { StyledCardList } from "./CardList.style";
-import axios from "axios";
 
-const apiKey = "3ca91bf17a8b671b2e8464cdec300f9f"; //temporary
-const baseUrl = "https://api.themoviedb.org/3/search/movie";
 const baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
-export const CardList = () => {
-  const [movieData, setMovieData] = useState(null);
-  useEffect(() => {
-    axios
-      .get(baseUrl, {
-        params: {
-          api_key: apiKey,
-          page: 1,
-          query: "Star Wars", //TODO: from input
-        },
-      })
-      .then((res) => setMovieData(res.data.results))
-      .catch((err) => console.log(err));
-  }, []);
+export const CardList = ({movieData}) => {
+  
 
-  console.log(movieData);
   return (
     <StyledCardList>
       {movieData &&
@@ -31,6 +15,7 @@ export const CardList = () => {
             <Card
               title={movie.title}
               image={baseImageUrl + movie.poster_path}
+              release={movie.release_date}
             />
           );
         })}
